ca5fd4d95ee8366539b4bfdec393655f3edadebb
srikara202 <srikara202@gmail.com>
2025-07-03 19:45:46 +1000

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Srikara",
+    author_email="srikara202@gmail.com",
+    packages=find_packages()
+)
